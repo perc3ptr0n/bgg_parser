3e9308cc814dec0bd85aea05c699c5464d10022d
perc3ptr0n <perc3ptr0n@gmail.com>
2020-02-11 19:03:25 +0200

    Rewrite saving to csv, add saving of link to the game and game id

---
+++ get_games_prices.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+chromedriver_path = './chromedriver'  # Change this to your own chromedriver path!
+webdriver = webdriver.Chrome(executable_path=chromedriver_path)
+
+# 'https://boardgamegeek.com/geekmarket/browse?objecttype=thing&objectid=174430&pageid=1'
+
+
--- load_games_list.py
+++ load_games_list.py
@@ -1,16 +1,14 @@
+from time import sleep
+import pandas as pd
 from selenium import webdriver
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.common.keys import Keys
-from time import sleep, strftime
-from random import randint
-
 
 # global variables
 choose_from = 'wishlist'  # Want in Trade, Want To Play, Want To Buy, Wishlist, The Hotness, TOP 10 by Category, TOP 100-1000 BGG etc
-games_list = []
-dump_path = 'games_list.txt'
-username = '****'
-password = '****'
+columns = ['game_name', 'link_on_bgg', 'game_id']
+games_table = pd.DataFrame(columns=columns)
+dump_path = 'games_info.csv'
+username = '****'  # enter here your username for BGG
+password = '****'  # enter here your password for BGG
 
 chromedriver_path = './chromedriver'  # Change this to your own chromedriver path!
 webdriver = webdriver.Chrome(executable_path=chromedriver_path)
@@ -19,7 +17,7 @@ webdriver = webdriver.Chrome(executable_path=chromedriver_path)
 def login_to_bgg(webdriver, username, password):
     sleep(1)
     webdriver.get('https://boardgamegeek.com/')
-    sleep(2)
+    sleep(1)
     sign_in_selector = '#global-header-outer > header > nav > div > div.global-header-nav > div > div.global-header-nav-primary-wrapper > ul > li.global-header-nav-session.global-header-nav-primary-separated.ng-scope > button'
 
     button_sign_in = webdriver.find_element_by_css_selector(sign_in_selector)
@@ -41,8 +39,7 @@ if choose_from == 'wishlist':
     link = 'https://boardgamegeek.com/wishlist/%s' % username
     webdriver.get(link)
     sleep(5)
-    last_page_num = int(
-        webdriver.find_element_by_css_selector('#collection > div:nth-child(3) > span > a').text)
+    last_page_num = int(webdriver.find_element_by_css_selector('#collection > div:nth-child(3) > span > a').text)
     for page in range(last_page_num):
 
         part_of_id_to_find = "results_objectname"
@@ -50,14 +47,15 @@ if choose_from == 'wishlist':
 
         for el in range(1, len(elements) + 1):
             game = webdriver.find_element_by_css_selector('#results_objectname%d' % el).text
-            print(game)
-            games_list.append(game)
-    sleep(5)
+            link = webdriver.find_element_by_css_selector('#results_objectname%d > a' % el)
+            href = link.get_attribute("href")
+            game_id = href.split('/')[-2]
+            game_record = [game, href, game_id]
+            new_record = pd.DataFrame([game_record[:]], columns=columns)
+            games_table = games_table.append([new_record], ignore_index=True)
+    sleep(3)
 # Here we could add other options
+games_table.to_csv(dump_path, mode='w', header=True)
 
-with open(dump_path, 'w') as f:
-    for game in games_list:
-        f.write(game + '\n')
-
-print('Collected %d board games. Saved them to %s' % (len(games_list), dump_path))
+print('Collected %d board games. Saved them to %s' % (games_table.shape[0], dump_path))
 webdriver.close()
